0f9d68e56e2601526e2582839be3c2704e2fef84
Amit gupta <guptaamit7571@gmail.com>
2026-07-29 01:16:15 +0530

    refactor: simplify store import path in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from "./store/store.js";
+import store from "./store/store";
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import { AuthLayout } from './components'
